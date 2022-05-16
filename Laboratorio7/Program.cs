292764cfea9ced1cb735a658973a23d29a61fc01
Henrique-Terres-Dell <Henrique.Terres@Dellteam.com>
2022-05-16 14:34:26 -0300

    Subindo Laboratorio7 csharp

---
+++ Laboratorio7/Program.cs
@@ -0,0 +1,12 @@
+﻿IEstadoBinario[] lista = new IEstadoBinario[2];
+lista[0] = new Lampada(110, 60);
+lista[0].Ligar();
+lista[1] = new TermometroEletrico();
+for(int i = 0; i < 2; i++) {
+    Console.WriteLine(lista[i].Estado);
+}
+
+Console.WriteLine("Ar condicionado: ");
+ArCondicionado ar = new ArCondicionado();
+ar.Ligar();
+Console.WriteLine(ar.Estado);
